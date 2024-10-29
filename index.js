8152265726d910851a91b7974fadaf8c0a3dbbe7
Zairaxx <brandontsegai@gmail.com>
2024-10-29 10:20:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//Functions
+
+function multiplyBy3(num){
+    let value = num * 3;
+    return value
+}
+
+console.log(multiplyBy3(10));
+
+function addNumbers(a,b){
+    let answer = a+b;
+    return answer;
+}
+
+function createArray(a,b,c){
+    return [a,b,c]
+}
+
+let fruits = createArray("Banan","Äpple","Mango");
+console.log(fruits);
+console.log(addNumbers("6",14));
+
+
+
+// Exempel på loop
